d47f9d0afe1abfde989da953ac07f799b25fcb32
BlaazH <mahamaneyacouba88@gmail.com>
2025-04-15 23:19:51 +0000

    projet initial

---
+++ script/operations.js
@@ -0,0 +1,16 @@
+/*Formation java script*/
+
+//types de données
+
+const PI = 22 / 7;
+console.log(typeof PI, PI);
+
+let Entier = 20;
+
+console.log(typeof Entier, Entier);
+
+Blaaz = "Mahamane";
+console.log(typeof Blaaz, Blaaz);
+
+let Tab = [10, 20, 3.5];
+console.log(typeof Tab, Tab);
