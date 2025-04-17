5cca4cc173078fb508d2313b49ac43aae38d57f4
BlaazH <mahamaneyacouba88@gmail.com>
2025-04-17 22:02:21 +0000

    execution d'une fonction

--- script/operations.js
+++ script/operations.js
@@ -1,17 +1,12 @@
-//lire et afficher le contenu d'une balise H1
+function calculer() {
+  console.log("effectuer un calcul");
+  const FirstNumberinput = document.getElementById("firstNumber");
+  const valueOfFirstNumberinput = FirstNumberinput.value;
+  console.log(valueOfFirstNumberinput);
+  const secondNumberinput = document.getElementById("secondNumber");
+  const valueOfsecondNumberinput = secondNumberinput.value;
+  console.log(valueOfsecondNumberinput);
 
-const ContentsOfH1Tags = document.getElementsByTagName("h1");
-
-// Afficher le contenu d'une balise H1
-console.log(ContentsOfH1Tags);
-
-//   Afficher le contenu de la premiere Balise H1
-
-const FirstH1 = ContentsOfH1Tags[0];
-
-console.log(FirstH1.innerHTML);
-
-// Libellé du button
-
-const button = document.getElementById("calculer");
-console.log(button.innerHTML);
+  result = 1 * valueOfFirstNumberinput + 1 * valueOfsecondNumberinput;
+  console.log(result);
+}
