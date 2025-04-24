f801fd55e9a921afbba297975827b1e268616a35
BlaazH <mahamaneyacouba88@gmail.com>
2025-04-24 10:57:22 +0000

    js

--- script/operations.js
+++ script/operations.js
@@ -1,1 +1,18 @@
+function calculer(event) {
+  event.preventDefault();
+  console.log("effectuer un calcul");
+  const premierNombreInput = document.getElementById("premierNombre");
+  const premierNombre = premierNombreInput.value;
+  console.log(premierNombre);
+
+  const secondNombreInput = document.getElementById("secondNombre");
+  const secondNombre = secondNombreInput.value;
+  console.log(secondNombre);
+
+  const resultat = 1 * premierNombre + 1 * secondNombre;
+  console.log(resultat);
+
+  document.getElementById("resultat").innerHTML = resultat;
+}
+
 alert("salut");
