2818d2ddc22fa2b6a72583f3eee1a59bb833ede6
BlaazH <mahamaneyacouba88@gmail.com>
2025-04-24 01:05:59 +0000

    er

--- script/operations.js
+++ script/operations.js
@@ -1,12 +1,1 @@
-function calculer() {
-  console.log("effectuer un calcul");
-  const FirstNumberinput = document.getElementById("firstNumber");
-  const valueOfFirstNumberinput = FirstNumberinput.value;
-  console.log(valueOfFirstNumberinput);
-  const secondNumberinput = document.getElementById("secondNumber");
-  const valueOfsecondNumberinput = secondNumberinput.value;
-  console.log(valueOfsecondNumberinput);
-
-  result = 1 * valueOfFirstNumberinput + 1 * valueOfsecondNumberinput;
-  console.log(result);
-}
+alert("salut");
---
+++ tp.js
@@ -0,0 +1,21 @@
+
+alert("salut");
+
+function calculer(event) {
+    event.preventDefault();
+  console.log("effectuer un calcul");
+  const premierNombreInput = document.getElementById("premierNombre");
+  const premierNombre = premierNombreInput.value;
+  console.log(premierNombre);
+
+  const secondNombreInput = document.getElementById("secondNombre");
+  const secondNombre = secondNombreInput.value;
+  console.log(secondNombre);
+
+  const resultat = 1 * premierNombre + 1 * secondNombre;
+  console.log(resultat);
+
+  document.getElementById("resultat")= resultat;
+}
+
+alert("salut");
