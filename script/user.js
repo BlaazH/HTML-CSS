71970cdff151decd7a7686570b6b1e37963768a7
BlaazH <mahamaneyacouba88@gmail.com>
2025-05-07 14:56:47 +0000

    table

--- script/user.js
+++ script/user.js
@@ -1,12 +1,28 @@
+function handleResponse(response) {
+  return response.json();
+}
+
+const handleData = (data) => {
+  console.log(data);
+  let tableLines = "";
+  for (let index = 0; index < data.length; index++) {
+    const users = data[index];
+
+    const tableLine = `
+    <tr>
+    <td>${users.name}</td>
+        <td>${users.phone}</td>
+        <td>${users.email}</td>
+    
+    </tr>
+    `;
+    tableLines = tableLines + tableLine;
+  }
+  document.getElementById("users-tbody").innerHTML = tableLines;
+};
+
 function fetchusers() {
   fetch("https://jsonplaceholder.typicode.com/users")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      //json = javascript object notation
-      console.log(data);
-
-      return data;
-    });
+    .then((response) => handleResponse(response))
+    .then((data) => handleData(data));
 }
